1c9e4873ed71e0e35773b8c9ee1ac646616b8076
mohlecodeur <mohlecodeur@gmail.com>
2025-05-18 22:13:06 +0000

    prod

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['myicon.jpg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Gestion Bus Mali',
+        short_name: 'BusMali',
+        description: 'Application de gestion de bus au Mali',
+        theme_color: '#0f172a', // bleu foncé ou couleur de ton branding
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
   css: {
     postcss: {
-      config: false // Désactive la recherche automatique de PostCSS
+      config: false
     }
+  },
+  optimizeDeps: {
+    exclude: [
+      'swiper/css',
+      'swiper/css/navigation',
+      'swiper/css/pagination',
+      'leaflet/dist/leaflet.css'
+    ]
   }
-})
-optimizeDeps: {
-  exclude: [
-    'swiper/css',
-    'swiper/css/navigation',
-    'swiper/css/pagination',
-    'leaflet/dist/leaflet.css'
-  ]
-}
+});
