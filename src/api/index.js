3f840b26a7440cebd91a87c4c5935c93db6cc022
mohlecodeur <mohlecodeur@gmail.com>
2025-07-18 16:05:53 +0000

    voila

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // ====================================================================
 // CORRECTION : On force l'URL de base du backend
 // ====================================================================
-const baseURL = 'https://mybusback.onrender.com/api';
+const baseURL = 'https://mybusbackkelly.onrender.com/api';
 
 const api = axios.create({
   baseURL: baseURL,
